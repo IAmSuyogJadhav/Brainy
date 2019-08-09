4f37713ab5d4ff0dd93a281c17e661ce76c25b4d
IAmSuyogJadhav <suyog.jadhav1@gmail.com>
2019-08-09 20:50:21 +0530

    Big changes

--- app/routes.py
+++ app/routes.py
@@ -1,74 +1,101 @@
-from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
-from werkzeug.urls import url_parse
+from app import app
+from flask import render_template, flash, redirect, url_for, request, send_from_directory
 from werkzeug import secure_filename
-from app.utils import make_gif
+from app.utils import make_gif, read_img, save_nii
 import os
-
-# Routes are assigned to handlers (called view functions) using app.route
-# decorator. You can map the same function to more than one routes too.
+import re
+import numpy as np
 
 
 @app.route('/')
 @app.route('/index')
-@login_required  # To force login
 def index():
     files = [file for file in os.listdir(app.config['UPLOAD_FOLDER'])
-             if file.endswith('.mha')]
+             if file.endswith('.mmri')]
     return render_template('index.html', title='Home', files=files)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid Username or Password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-
-    return render_template('login.html', title='Sign-In', form=form)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('You have registered succesfully! Now login to continue.')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/upload')
 def upload_file():
-    return render_template('upload.html', title='Upload an MRI file')
+    return render_template('upload.html', title='Upload the MRI files')
 
 
 @app.route('/uploader', methods=['POST'])
 def upload_file_():
+    cause = ''  # For catching specific cause of error
+    modalities = ['t1', 't2w', 't1ce', 'flair']
+    pat = re.compile(r'[^\.]*\.(.*)')
+    formats = {m: pat.findall(request.files[m].filename)[0] for m in modalities}
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],
-                            secure_filename(f.filename)))
-        flash('File uploaded succesfully.')
+        try:
+            cause = 'while uploading the files. Ensure that the files'
+            ' are accessible and try again. '
+            for m in modalities:
+                f = request.files[m]
+                f.save(
+                 os.path.join(
+                   app.config['UPLOAD_FOLDER'],
+                   secure_filename(f"{request.form['name']}_{m}.{formats[m]}")
+                   )
+                )
+            flash('Files were uploaded succesfully.')
+
+            cause = 'while exporting the files into a single multimodal-MRI'
+            ' (.mmri) file. Make sure the uploaded files are valid MRI files'
+            ' and try again.'
+            img = np.array(
+             [
+               read_img(
+                 os.path.join(
+                   app.config['UPLOAD_FOLDER'],
+                   secure_filename(f"{request.form['name']}_{m}.{formats[m]}")
+                 )
+               )
+               for m in modalities
+             ]
+            )
+
+            np.save(os.path.join(
+              app.config['UPLOAD_FOLDER'],
+              secure_filename(f"{request.form['name']}")
+            ), img)
+
+            os.rename(
+                os.path.join(
+                    app.config['UPLOAD_FOLDER'],
+                    secure_filename(f"{request.form['name']}.npy")
+                    ),
+                os.path.join(
+                    app.config['UPLOAD_FOLDER'],
+                    secure_filename(f"{request.form['name']}.mmri")
+                    )
+                )
+            cause = 'while cleaning up the temporary files. Make sure this '
+            f'path is accessible and try again:{app.config["UPLOAD_FOLDER"]}'
+            [os.remove(
+              os.path.join(
+                app.config['UPLOAD_FOLDER'],
+                f'{request.form["name"]}_{m}.{formats[m]}'
+                )
+            ) for m in modalities]
+
+            cause = None
+
+        except Exception as e:
+            flash(
+                f'An error occured {cause}' if cause is not None else
+                'An unknown error occured.')
+            return f"""<div class="w3-container">
+              <h1 class="w3-xxxlarge w3-text-black"><b>Sorry Something Went Wrong.</b></h1>
+              <hr style="width:50px;border:5px solid red" class="w3-round">
+              <p>An error occured while uploading the MRI files. See below for more info.</p>
+              <br />
+              <h3 class="w3-xlarge w3-text-black"><b>Error Text:</b></h3>
+              <hr>
+              <p> {e} </p>
+              <a href='/upload'><h3 class="w3-xlarge w3-text-black">
+                <b>&lt; Go back and try again.</b></h3></a>
+            </div>"""
         return redirect(url_for('index'))
     return redirect(url_for('index'))
 
@@ -79,19 +106,28 @@ def analyze():
         file = request.form.get('files_dropdown')
         print(file)
         print(app.config["UPLOAD_FOLDER"])
-        try:
-            make_gif(f'{app.config["UPLOAD_FOLDER"]}/{file}')
-            success = True
-            error = None
-        except Exception as e:
-            success = False
-            error = str(e)
+        # try:
+        out_file = make_gif(file)
+        success = True
+        error = None
+        # except Exception as e:
+        #     success = False
+        #     error = str(e)
 
         return render_template('analyze.html', title='Results', success=success,
-                               error=error, folder=app.config["UPLOAD_FOLDER"])
+                               file=out_file, error=error, folder=app.config["UPLOAD_FOLDER"])
+    elif request.method == 'GET':
+        flash('Select a MMRI file from the list or add your own.')
+        return redirect(url_for('index'))
 
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+@app.route('/download-mask/<file>/<mod>', methods=['GET'])
+def download(file, mod):
+    if request.method == 'GET':
+        mods = {'t1': 0, 't2': 1, 't1ce': 2, 'flair':3}
+
+        img = np.load(f'{app.config["UPLOAD_FOLDER"]}/{file}.npy')
+        save = img[mods[mod]]
+        name = f'{file[:-5]}_{mod}.nii.gz'
+        save_nii(save, name)
+        return redirect(url_for('static', filename=app.config['EXPORT_FOLDER_REL']+name), code=301)
--- app/utils.py
+++ app/utils.py
@@ -1,16 +1,42 @@
 import imageio
-# import nibabel as nib  # for nii.gz
-import SimpleITK as io  # for .mha
+import SimpleITK as sitk
 from app import app
-from flask import url_for
 from keras.models import load_model
 from scipy.ndimage import zoom
 from functools import partial
-from keras.optimizers import Adam
+import tensorflow as tf
 import numpy as np
 from keras import backend as K
+import cv2
+import os
+
 K.set_image_dim_ordering('tf')
 K.set_image_data_format('channels_first')
+
+
+def read_img(img_path):
+    return sitk.GetArrayFromImage(sitk.ReadImage(img_path))
+
+
+def preprocess(img, out_shape=None, labels=False):
+    if out_shape is not None:
+        factors = (out_shape[0] / 155, out_shape[1] / 240, out_shape[2] / 240)
+        img_ = zoom(img, factors)
+    else:
+        img_ = img.copy()
+
+    if not labels:
+        mean = img_.mean()
+        std = img_.std()
+        img_ = (img_ - mean) / std
+    return img_
+
+
+def postprocess(mask):
+    kernel = np.ones((3, 3))
+    for i in range(mask.shape[0]):
+        for j in range(mask.shape[1]):
+            mask[i, j, ...] = cv2.dilate(mask[i, j, ...], kernel, iterations=1)
 
 
 def dice_coefficient(y_true, y_pred, smooth=1.):
@@ -49,52 +75,112 @@ def label_wise_dice_coefficient(y_true, y_pred, label_index):
 
 def get_label_dice_coefficient_function(label_index):
     f = partial(label_wise_dice_coefficient, label_index=label_index)
-    f._setattr('name', 'label{0}_dice_coef'.format(label_index))
+    f.__setattr__('__name__', 'label_{0}_dice_coef'.format(label_index))
     return f
 
 
 dice_coef = dice_coefficient
 dice_coef_loss = dice_coefficient_loss
 
-model = load_model(app.config['MODEL'], custom_objects={'dice_coefficient':dice_coef,'dice_coefficient_loss': dice_coef_loss})
+
+def load_old_model(model_file):
+    print("Loading pre-trained model")
+    custom_objects = {'dice_coefficient_loss': dice_coefficient_loss, 'dice_coefficient': dice_coefficient,
+                      'dice_coef': dice_coef, 'dice_coef_loss': dice_coef_loss,
+                      'weighted_dice_coefficient': weighted_dice_coefficient,
+                      'weighted_dice_coefficient_loss': weighted_dice_coefficient_loss}
+    try:
+        from keras_contrib.layers import InstanceNormalization
+        custom_objects["InstanceNormalization"] = InstanceNormalization
+    except ImportError:
+        pass
+    try:
+        return load_model(model_file, custom_objects=custom_objects)
+    except ValueError as error:
+        if 'InstanceNormalization' in str(error):
+            raise ValueError(str(error) + "\n\nPlease install keras-contrib to use InstanceNormalization:\n"
+                                          "'pip install git+https://www.github.com/keras-team/keras-contrib.git'")
+        else:
+            raise error
 
 
-def make_gif(PATH):
-    # For .mha
-    img = io.ReadImage(PATH)
-    img = io.GetArrayFromImage(img)
+model = load_old_model(app.config['MODEL'])
+graph = tf.get_default_graph()
 
-    # For .nii.gz
-    # img = nib.load(PATH)
-    # img = img.get_fdata()
 
-    def get(i, axes):
-        if axes == 0:
-            img1 = img[i, :, :]
-            img1[0, 0] = 1
-        elif axes == 1:
-            img1 = img[:, i, :]
-            img1[0, 0] = 1
-        elif axes == 2:
-            img1 = img[:, :, i]
-            img1[0, 0] = 1
+def make_gif(file):
+    img = np.load(f'{app.config["UPLOAD_FOLDER"]}/{file}')
+    img = np.array([
+        preprocess(img[m],  out_shape=(80, 96, 64), labels=True)
+        for m in range(4)
+        ])
+    # Setting labels to True so that normalization doesn't happen right away
+    # and make the GIFs look bad.
+
+    def get(i, image):
+        img1 = image[2][i, ...]
+        img1[0, 0] = 1
         return img1
 
     path = app.config['UPLOAD_FOLDER']
-    imageio.mimsave(f'{path}/gifs/x.gif',
-                    [get(i, axes=0) for i in range(img.shape[0])], fps=20)
-    imageio.mimsave(f'{path}/gifs/y.gif',
-                    [get(i, axes=1) for i in range(img.shape[1])], fps=20)
-    imageio.mimsave(f'{path}/gifs/z.gif',
-                    [get(i, axes=2) for i in range(img.shape[2])], fps=20)
+    imageio.mimsave(f'{path}/gifs/t1.gif',
+                    [get(i, img) for i in range(img[0].shape[0])], fps=10)
+    imageio.mimsave(f'{path}/gifs/t2.gif',
+                    [get(i, img) for i in range(img[1].shape[0])], fps=10)
+    imageio.mimsave(f'{path}/gifs/t1ce.gif',
+                    [get(i, img) for i in range(img[2].shape[0])], fps=10)
+    imageio.mimsave(f'{path}/gifs/flair.gif',
+                    [get(i, img) for i in range(img[3].shape[0])], fps=10)
 
-    pad = zoom(img, (.776, .5, .5))
-    img_out = model.predict(pad[None, None, ...]).squeeze()
+    pad = preprocess(img)
+
+    with graph.as_default():
+        masks = model.predict(pad[None, ...]).squeeze() >= 0.5
 
     path = app.config['UPLOAD_FOLDER']
-    imageio.mimsave(f'{path}/gifs/x_out.gif',
-                    [get(i, axes=0) for i in range(img_out.shape[0])], fps=20)
-    imageio.mimsave(f'{path}/gifs/y_out.gif',
-                    [get(i, axes=1) for i in range(img_out.shape[1])], fps=20)
-    imageio.mimsave(f'{path}/gifs/z_out.gif',
-                    [get(i, axes=2) for i in range(img_out.shape[2])], fps=20)
+
+    # Coloring the segmentation
+    colors=[
+        np.array([255, 0, 0], dtype=np.uint8),  # NCR/NET: label 1, slice no. 0
+        np.array([0, 255, 0], dtype=np.uint8),  # ED: label 2, slice no. 1
+        np.array([0, 0, 255], dtype=np.uint8)  # ET: label 4, slice no. 2
+    ]
+
+    img_out = np.stack([img, img, img], axis=-1).astype(np.uint8)
+
+    masks = np.stack([masks, masks, masks], axis=-1).astype(np.uint8)
+    for i in range(3):
+        color = colors[i]
+        masks[i] *= color
+
+    alpha = 0.4
+    for m in range(img_out.shape[0]):
+        for i in range(3):
+            cv2.addWeighted(masks[i], alpha, img_out[m], 1-alpha, 0, img_out[m])
+
+    imageio.mimsave(f'{path}/gifs/t1_seg.gif',
+                    [get(i, img_out) for i in range(img_out[0].shape[0])], fps=10)
+    imageio.mimsave(f'{path}/gifs/t2_seg.gif',
+                    [get(i, img_out) for i in range(img_out[1].shape[0])], fps=10)
+    imageio.mimsave(f'{path}/gifs/t1ce_seg.gif',
+                    [get(i, img_out) for i in range(img_out[2].shape[0])], fps=10)
+    imageio.mimsave(f'{path}/gifs/flair_seg.gif',
+                    [get(i, img_out) for i in range(img_out[3].shape[0])], fps=10)
+
+    name = f'{file[:-5]}_mask'
+    np.save(os.path.join(app.config['UPLOAD_FOLDER'], name), img_out)
+    return name
+
+
+def save_nii(file, name):
+    """
+    Saves input numpy array in nii.gz file.
+    """
+    dir = app.config['EXPORT_FOLDER']
+
+    writer = sitk.ImageFileWriter()
+    os.makedirs(dir, exist_ok=True)
+    path = os.path.join(dir, f'{name}.nii.gz' if not name.endswith('nii.gz') else name)
+    writer.SetFileName(path)
+    writer.Execute(sitk.GetImageFromArray(file))
+    print(f"Succesfully saved {path}")
